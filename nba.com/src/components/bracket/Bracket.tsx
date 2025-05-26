25efb56e5b87d813c7c8bbd635e86fb042c68d17
Key'n Brosdahl <106933331+Keynb23@users.noreply.github.com>
2025-05-26 18:32:19 -0500

    added matchup containers, fixed color scheme, and spacing.

--- nba.com/src/components/bracket/Bracket.tsx
+++ nba.com/src/components/bracket/Bracket.tsx
@@ -43,141 +43,187 @@ const Bracket: React.FC = () => {
       <div className="BracketGrid">
         <div className="WestConf">
           <div className="WFR">
-            <div id="Thunder" className="grid-item westFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612760/primary/L/logo.svg" alt="Thunder Logo" className="team-logo"/>
-              <span>1 Thunder</span>
-            </div>
-            <div id="Griz" className="grid-item westFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612763/primary/L/logo.svg" alt="Grizzlies Logo" className="team-logo"/>
-              <span>8 Grizzlies</span>
-            </div>
-            <div id="Nuggets" className="grid-item westFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612743/primary/L/logo.svg" alt="Nuggets Logo" className="team-logo"/>
-              <span>4 Nuggets</span>
-            </div>
-            <div id="Clippers" className="grid-item westFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612746/primary/L/logo.svg" alt="Clippers Logo" className="team-logo"/>
-              <span>5 Clippers</span>
-            </div>
-            <div id="Laker" className="grid-item westFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612747/primary/L/logo.svg" alt="Lakers Logo" className="team-logo"/>
-              <span>3 Lakers</span>
-            </div>
-            <div id="Wolves" className="grid-item westFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612750/primary/L/logo.svg" alt="Timberwolves Logo" className="team-logo"/>
-              <span>6 Timberwolves</span>
-            </div>
-            <div id="Rockets" className="grid-item westFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612745/primary/L/logo.svg" alt="Rockets Logo" className="team-logo"/>
-              <span>2 Rockets</span>
-            </div>
-            <div id="Warriors" className="grid-item westFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612744/primary/L/logo.svg" alt="Warriors Logo" className="team-logo"/>
-              <span>7 Warriors</span>
-            </div>
-          </div>
 
-          <div className="WCSF">
-            <div id="Thunder" className="grid-item west-wcsf">
-              <img src="https://cdn.nba.com/logos/nba/1610612760/primary/L/logo.svg" alt="Thunder Logo" className="team-logo"/>
-              <span>1 Thunder</span>
-            </div>
-            <div id="Nuggets" className="grid-item west-wcsf">
-              <img src="https://cdn.nba.com/logos/nba/1610612743/primary/L/logo.svg" alt="Nuggets Logo" className="team-logo"/>
-              <span>4 Nuggets</span>
+            <div className='matchup-containers'>
+              <div id="Thunder" className="grid-item westFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612760/primary/L/logo.svg" alt="Thunder Logo" className="team-logo"/>
+                <span>1 Thunder</span>
+              </div>
+              <div id="Griz" className="grid-item westFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612763/primary/L/logo.svg" alt="Grizzlies Logo" className="team-logo"/>
+                <span>8 Grizzlies</span>
+              </div>
             </div>
 
-            <div id="Wolves" className="grid-item west-wcsf">
-              <img src="https://cdn.nba.com/logos/nba/1610612750/primary/L/logo.svg" alt="Timberwolves Logo" className="team-logo"/>
-              <span>6 Timberwolves</span>
-            </div>
-            <div id="Warriors" className="grid-item west-wcsf">
-              <img src="https://cdn.nba.com/logos/nba/1610612744/primary/L/logo.svg" alt="Warriors Logo" className="team-logo"/>
-              <span>7 Warriors</span>
-            </div>
-          </div>
+            <div className='matchup-containers'>
+              <div id="Nuggets" className="grid-item westFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612743/primary/L/logo.svg" alt="Nuggets Logo" className="team-logo"/>
+                <span>4 Nuggets</span>
+              </div>
+              <div id="Clippers" className="grid-item westFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612746/primary/L/logo.svg" alt="Clippers Logo" className="team-logo"/>
+                <span>5 Clippers</span>
+              </div>
+              </div>
 
-          <div className="WCF">
-            <div id="Thunder" className="grid-item west-wcf">
-              <img src="https://cdn.nba.com/logos/nba/1610612760/primary/L/logo.svg" alt="Thunder Logo" className="team-logo"/>
-              <span>1 Thunder</span>
-            </div>
-            <div id="Wolves" className="grid-item west-wcf">
-              <img src="https://cdn.nba.com/logos/nba/1610612750/primary/L/logo.svg" alt="Timberwolves Logo" className="team-logo"/>
-              <span>6 Timberwolves</span>
-            </div>
-          </div>
-        </div>
+            <div className='matchup-containers'>
+              <div id="Laker" className="grid-item westFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612747/primary/L/logo.svg" alt="Lakers Logo" className="team-logo"/>
+                <span>3 Lakers</span>
+              </div>
+              <div id="Wolves" className="grid-item westFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612750/primary/L/logo.svg" alt="Timberwolves Logo" className="team-logo"/>
+                <span>6 Timberwolves</span>
+              </div>
+              </div>
 
-        <div className="EastConf">
-          <div className="ECF">
-            <div id="Knicks" className="grid-item east-ecf">
-              <img src="https://cdn.nba.com/logos/nba/1610612752/primary/L/logo.svg" alt="Knicks Logo" className="team-logo"/>
-              <span>3 Knicks</span>
-            </div>
-            <div id="Pacers" className="grid-item east-ecf">
-              <img src="https://cdn.nba.com/logos/nba/1610612754/primary/L/logo.svg" alt="Pacers Logo" className="team-logo"/>
-              <span>4 Pacers</span>
-            </div>
-          </div>
 
-          <div className="ECSF">
-            <div id="Cavs" className="grid-item east-ecsf">
-              <img src="https://cdn.nba.com/logos/nba/1610612739/primary/L/logo.svg" alt="Cavaliers Logo" className="team-logo"/>
-              <span>1 Cavaliers</span>
+            <div className='matchup-containers'>
+              <div id="Rockets" className="grid-item westFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612745/primary/L/logo.svg" alt="Rockets Logo" className="team-logo"/>
+                <span>2 Rockets</span>
+              </div>
+              <div id="Warriors" className="grid-item westFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612744/primary/L/logo.svg" alt="Warriors Logo" className="team-logo"/>
+                <span>7 Warriors</span>
+              </div>
             </div>
-            <div id="Pacers" className="grid-item east-ecsf">
-              <img src="https://cdn.nba.com/logos/nba/1610612754/primary/L/logo.svg" alt="Pacers Logo" className="team-logo"/>
-              <span>4 Pacers</span>
             </div>
 
-            <div id="Knicks" className="grid-item east-ecsf">
-              <img src="https://cdn.nba.com/logos/nba/1610612752/primary/L/logo.svg" alt="Knicks Logo" className="team-logo"/>
-              <span>3 Knicks</span>
-            </div>
-            <div id="Celtics" className="grid-item east-ecsf">
-              <img src="https://cdn.nba.com/logos/nba/1610612738/primary/L/logo.svg" alt="Celtics Logo" className="team-logo"/>
-              <span>2 Celtics</span>
-            </div>
-          </div>
 
-          <div className="EFR">
-            <div id="Cavs" className="grid-item eastFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612739/primary/L/logo.svg" alt="Cavaliers Logo" className="team-logo"/>
-              <span>1 Cavaliers</span>
+            <div className="WCSF">
+
+            <div className='matchup-containers'>
+              <div id="Thunder" className="grid-item west-wcsf">
+                <img src="https://cdn.nba.com/logos/nba/1610612760/primary/L/logo.svg" alt="Thunder Logo" className="team-logo"/>
+                <span>1 Thunder</span>
+              </div>
+              <div id="Nuggets" className="grid-item west-wcsf">
+                <img src="https://cdn.nba.com/logos/nba/1610612743/primary/L/logo.svg" alt="Nuggets Logo" className="team-logo"/>
+                <span>4 Nuggets</span>
+              </div>
+              </div>
+
+
+            <div className='matchup-containers'>
+              <div id="Wolves" className="grid-item west-wcsf">
+                <img src="https://cdn.nba.com/logos/nba/1610612750/primary/L/logo.svg" alt="Timberwolves Logo" className="team-logo"/>
+                <span>6 Timberwolves</span>
+              </div>
+              <div id="Warriors" className="grid-item west-wcsf">
+                <img src="https://cdn.nba.com/logos/nba/1610612744/primary/L/logo.svg" alt="Warriors Logo" className="team-logo"/>
+                <span>7 Warriors</span>
+              </div>
             </div>
-            <div id="Heat" className="grid-item eastFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612748/primary/L/logo.svg" alt="Heat Logo" className="team-logo"/>
-              <span>8 Heat</span>
             </div>
-            <div id="Pacers" className="grid-item eastFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612754/primary/L/logo.svg" alt="Pacers Logo" className="team-logo"/>
-              <span>4 Pacers</span>
-            </div>
-            <div id="Bucks" className="grid-item eastFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612749/primary/L/logo.svg" alt="Bucks Logo" className="team-logo"/>
-              <span>5 Bucks</span>
-            </div>
-            <div id="Knicks" className="grid-item eastFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612752/primary/L/logo.svg" alt="Knicks Logo" className="team-logo"/>
-              <span>3 Knicks</span>
-            </div>
-            <div id="Pistons" className="grid-item eastFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612765/primary/L/logo.svg" alt="Pistons Logo" className="team-logo"/>
-              <span>6 Pistons</span>
-            </div>
-            <div id="Celtics" className="grid-item eastFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612738/primary/L/logo.svg" alt="Celtics Logo" className="team-logo"/>
-              <span>2 Celtics</span>
-            </div>
-            <div id="Magic" className="grid-item eastFR">
-              <img src="https://cdn.nba.com/logos/nba/1610612738/primary/L/logo.svg" alt="Magic Logo" className="team-logo"/>
-              <span>7 Magic</span>
+
+            <div className="WCF">
+
+            <div className='matchup-containers'>
+              <div id="Thunder" className="grid-item west-wcf">
+                <img src="https://cdn.nba.com/logos/nba/1610612760/primary/L/logo.svg" alt="Thunder Logo" className="team-logo"/>
+                <span>1 Thunder</span>
+              </div>
+              <div id="Wolves" className="grid-item west-wcf">
+                <img src="https://cdn.nba.com/logos/nba/1610612750/primary/L/logo.svg" alt="Timberwolves Logo" className="team-logo"/>
+                <span>6 Timberwolves</span>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="grid-item finals-tbd">tbd</div>
+          <div className="EastConf">
+            <div className="ECF">
+
+            <div className='matchup-containers'>
+              <div id="Knicks" className="grid-item east-ecf">
+                <img src="https://cdn.nba.com/logos/nba/1610612752/primary/L/logo.svg" alt="Knicks Logo" className="team-logo"/>
+                <span>3 Knicks</span>
+              </div>
+              <div id="Pacers" className="grid-item east-ecf">
+                <img src="https://cdn.nba.com/logos/nba/1610612754/primary/L/logo.svg" alt="Pacers Logo" className="team-logo"/>
+                <span>4 Pacers</span>
+              </div>
+            </div>
+            </div>
+
+            <div className="ECSF">
+
+            <div className='matchup-containers'>
+              <div id="Cavs" className="grid-item east-ecsf">
+                <img src="https://cdn.nba.com/logos/nba/1610612739/primary/L/logo.svg" alt="Cavaliers Logo" className="team-logo"/>
+                <span>1 Cavaliers</span>
+              </div>
+              <div id="Pacers" className="grid-item east-ecsf">
+                <img src="https://cdn.nba.com/logos/nba/1610612754/primary/L/logo.svg" alt="Pacers Logo" className="team-logo"/>
+                <span>4 Pacers</span>
+              </div>
+            </div>
+
+            <div className='matchup-containers'>
+              <div id="Knicks" className="grid-item east-ecsf">
+                <img src="https://cdn.nba.com/logos/nba/1610612752/primary/L/logo.svg" alt="Knicks Logo" className="team-logo"/>
+                <span>3 Knicks</span>
+              </div>
+              <div id="Celtics" className="grid-item east-ecsf">
+                <img src="https://cdn.nba.com/logos/nba/1610612738/primary/L/logo.svg" alt="Celtics Logo" className="team-logo"/>
+                <span>2 Celtics</span>
+              </div>
+            </div>
+            </div>
+
+
+            <div className="EFR">
+
+            <div className='matchup-containers'>
+              <div id="Cavs" className="grid-item eastFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612739/primary/L/logo.svg" alt="Cavaliers Logo" className="team-logo"/>
+                <span>1 Cavaliers</span>
+              </div>
+              <div id="Heat" className="grid-item eastFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612748/primary/L/logo.svg" alt="Heat Logo" className="team-logo"/>
+                <span>8 Heat</span>
+              </div>
+              </div>
+
+            <div className='matchup-containers'>
+              <div id="Pacers" className="grid-item eastFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612754/primary/L/logo.svg" alt="Pacers Logo" className="team-logo"/>
+                <span>4 Pacers</span>
+              </div>
+              <div id="Bucks" className="grid-item eastFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612749/primary/L/logo.svg" alt="Bucks Logo" className="team-logo"/>
+                <span>5 Bucks</span>
+              </div>
+            </div>
+
+            <div className='matchup-containers'>
+              <div id="Knicks" className="grid-item eastFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612752/primary/L/logo.svg" alt="Knicks Logo" className="team-logo"/>
+                <span>3 Knicks</span>
+              </div>
+              <div id="Pistons" className="grid-item eastFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612765/primary/L/logo.svg" alt="Pistons Logo" className="team-logo"/>
+                <span>6 Pistons</span>
+              </div>
+              </div>
+
+            <div className='matchup-containers'>
+              <div id="Celtics" className="grid-item eastFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612738/primary/L/logo.svg" alt="Celtics Logo" className="team-logo"/>
+                <span>2 Celtics</span>
+              </div>
+              <div id="Magic" className="grid-item eastFR">
+                <img src="https://cdn.nba.com/logos/nba/1610612738/primary/L/logo.svg" alt="Magic Logo" className="team-logo"/>
+                <span>7 Magic</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+        <div className="grid-item finals">
+          <img src="https://cdn.nba.com/logos/playoffs/2024/L/finals.svg" alt="Finals Logo" className="finals-logo"/>
+        </div>
       </div>
     </div>
   );
